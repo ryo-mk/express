696d9986e9f913270194454b65d125add061b914
maki <mkr.fb-66@cameo.plala.or.jp>
2024-01-27 21:41:38 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,25 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const cors = require('cors'); // corsミドルウェアを追加
+const cors = require("cors"); // corsミドルウェアを追加
+require("dotenv").config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "******";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
 router.use(cors());
 
-router.get('/', async (req, res) => {
-// データベース、コレクションを指定
-const database = client.db('notes');
-const notes = database.collection('notes');
+router.get("/", async (req, res) => {
+  // データベース、コレクションを指定
+  const database = client.db("notes");
+  const notes = database.collection("notes");
 
+  // 全てのドキュメントを取得
+  const note = await notes.find({}).toArray();
 
-// 全てのドキュメントを取得
-const note = await notes.find({}).toArray();
-
-res.json(note);
-})
+  res.json(note);
+});
 
 module.exports = router;
